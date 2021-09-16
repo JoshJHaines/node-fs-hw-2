e260bbf78979a4bcf92c082c417aea5a3fa7a006
JoshuaJHaines <joshjhaines3@gmail.com>
2021-09-15 22:03:53 -0400

    route created that makes a txt file

--- README.md
+++ README.md
@@ -10,12 +10,10 @@
 
 ## URLs and CRUD
 - [ ] Using fs module inside createServer callback:
-*If request.url === “/create-directory”
-- [ ] create a directory called content using fs method and it should log 'content folder created'
-* If request.url === “/create-text”
-- [ ] create a file using fs method called randomText.txt that lives outside the content directory
-- [ ] randomText.txt should be created using fs method and given just a short string of any data you want to put in it, using fs method
-- [ ] when you create the randomText.txt file you should also log 'randomtext.txt created' in the terminal.
+  
+- [ ] If request.url === “/create-directory” create a directory called content using fs method and it should log 'content folder created'
+  
+- [x] If request.url === “/create-text” create a file using fs method called randomText.txt that lives outside the content directory randomText.txt should be created using fs method and given just a short string of any data you want to put in it, using fs method when you create the randomText.txt file you should also log 'randomtext.txt created' in the terminal.
 *if request.url === “/new-folder-and-file”
 - [ ] write the randomText.txt data to a new file called verbage.txt inside the content folder and log 'verbage.txt created'
 - [ ] Now create a separate setTimeout function that after 7 seconds deletes the content directory using fs methods (inside /new-folder-and-file)
--- server.js
+++ server.js
@@ -1,10 +1,65 @@
 const http = require("http");
 
+const fs = require("fs")
+
 const port = 3000;
 
 const server = http.createServer(function (request, response) {
-	response.end("hi class");
-});
+	
+    if (request.url === "/") {
+		fs.readFile("./public/index.html", function (error, data) {
+			if (error) {
+				return response.end(`${error}`);
+			} else {
+				response.writeHead(200, { "Content-type": "text/html" });
+				response.write(data);
+				return response.end();
+			}
+		});
+	}
+    // if (request.url === "create-directory"){
+    //     fs.mkdir(".", "content", function(error) {
+    //         if (error) {
+	// 			return response.end(`${error}`);
+    //         } else {
+    //             response.end('content folder created')
+    //         }
+    //     });
+    // }
+    if (request.url === "/create-text" && request.method === "POST"){
+		let body = ""
+
+		request.on("data", function (data){
+			body += data.toString()
+		})
+		
+		request.on("end", function(){
+			let parsedBody = JSON.parse(body)
+
+			fs.writeFile(parsedBody.fileName, parsedBody.message,function (error) {
+				if (error){
+					response.writeHead(400)
+					response.end(`${error}`)
+					response.end()
+				} else {
+					//default, if successful will show 200
+					// response.writeHead(200, { "Content-type": "text/html" });
+					console.log("randomText.txt created")
+					response.end('randomText.txt created');
+				};
+			});
+			console.log(parsedBody)
+		})
+		
+	} 
+})
+
+
+
+
+
+
+
 
 server.listen(port);
 console.log(`Server is now up and running @ port: ${port}`);
